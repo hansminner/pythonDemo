552cbed487719942209be2ceff6a5f1d393f54a8
hansminner <360312085@qq.com>
2017-10-19 23:22:28 +0800

    迭代器 生成器

--- demo2.py
+++ demo2.py
@@ -63,3 +63,59 @@ else:
         print("the number not can be divided with no remainder by 2 and 3")
 """
 
+# 迭代器 iter() 和 next()。
+# 迭代器对象从集合的第一个元素开始访问，直到所有的元素被访问完结束。迭代器只能往前不会后退。
+
+# 迭代器对象可以使用常规for语句进行遍历：
+"""
+list=[1,2,3,4]
+it = iter(list)    # 创建迭代器对象
+for x in it:
+    print (x, end=" ")
+"""
+# 也可以使用 next() 函数：
+"""
+import sys  # 引入 sys 模块
+
+list = [1, 2, 3, 4]
+it = iter(list)  # 创建迭代器对象
+
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        sys.exit()
+"""
+
+# 生成器 yield 使用了 yield 的函数被称为生成器（generator）
+# 跟普通函数不同的是，生成器是一个返回迭代器的函数，只能用于迭代操作，更简单点理解生成器就是一个迭代器。
+# 在调用生成器运行的过程中，每次遇到 yield 时函数会暂停并保存当前所有的运行信息，返回yield的值。
+# 并在下一次执行 next()方法时从当前位置继续运行。
+"""
+一个函数 f，f 返回一个 list，这个 list 是动态计算出来的（不管是数学上的计算还是逻辑上的读取格式化），
+并且这个 list 会很大（无论是固定很大还是随着输入参数的增大而增大），
+这个时候，我们希望每次调用这个函数并使用迭代器进行循环的时候一个一个的得到每个 list 元素而不是直接得到一个完整的 list 来节省内存，
+这个时候 yield 就很有用。
+"""
+
+"""
+import sys
+
+def fibonacci(n):  # 生成器函数 - 斐波那契
+    a, b, counter = 0, 1, 0
+    while True:
+        if (counter > n):
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+
+
+f = fibonacci(10)  # f 是一个迭代器，由生成器返回生成
+
+while True:
+    try:
+        print(next(f), end=" ")
+    except StopIteration:
+        sys.exit()
+"""
